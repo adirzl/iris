69b7e2695c67665b223bdec2d6a990de6d57461e
adirzl <adirizalnur@gmail.com>
2021-01-18 17:00:14 +0700

    perbaikan landing page, menu dan sub menu

--- routes/web.php
+++ routes/web.php
@@ -25,14 +25,19 @@ Route::get('/home', 'HomeController@index')->name('home');
 // Landing Routes
 Route::get('/landing', 'LandingController@index')->name('landing');
 Route::get('/{id}/detail', 'LandingController@detail')->name('landingdetail');
+Route::get('/tugas_wew_kk', 'LandingController@tugas_wew_kk');
+Route::get('/perencanaan_bisnis_bank', 'LandingController@perencanaan_bisnis_bank');
+Route::get('/pengembangan_organisasi', 'LandingController@pengembangan_organisasi');
+Route::get('/profil', 'LandingController@profil');
+Route::get('/visi_misi', 'LandingController@visi_misi');
+Route::get('/sekapur_sirih', 'LandingController@sekapur_sirih');
+Route::get('/arsip_dokumen', 'LandingController@arsip_dokumen');
 
 Route::get('/tentang_kk', 'LandingController@tentang_kk');
 Route::get('/sekilas_kk', 'LandingController@sekilas_kk');
 Route::get('/struktur_kk', 'LandingController@struktur_kk');
 Route::get('/comp_prof_kk', 'LandingController@comp_prof_kk');
 Route::get('/comp_prof_kk_detail/{id}', 'LandingController@comp_prof_kk_detail');
-Route::get('/tugas_wew_kk', 'LandingController@tugas_wew_kk');
-Route::get('/tentang_kinke', 'LandingController@tentang_kinke');
 Route::get('/report_sumber', 'LandingController@report_sumber');
 Route::get('/kajian_kinke', 'LandingController@kajian_kinke');
 Route::get('/regulasi', 'LandingController@regulasi');
@@ -51,4 +56,4 @@ Route::get('/report_kuisioner_manrisk/export/{id}', 'LandingController@export');
 Route::get('/report_kuisioner_kepatuhan', 'LandingController@report_kuisioner_kepatuhan');
 Route::get('/report_kuisioner_kepatuhan/export/{id}', 'LandingController@export');
 Route::get('/report_kuisioner_audit', 'LandingController@report_kuisioner_audit');
-Route::post('/ceklogin','LandingController@cekLogin')->name('ceklogin');
+Route::post('/ceklogin', 'LandingController@cekLogin')->name('ceklogin');
